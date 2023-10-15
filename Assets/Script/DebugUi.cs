14a1bd9e48d9ae4928ac93e5495c4d95990cee59
ojh1158 <108050056+ojh1158@users.noreply.github.com>
2023-10-15 20:28:04 +0900

    Bugfix

--- Assets/Script/DebugUi.cs
+++ Assets/Script/DebugUi.cs
@@ -8,15 +8,10 @@ public class DebugUi : MonoBehaviour
     public TMP_Text text;
 
     public static string Debug;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        text.text = Debug;
+        // 
+        // text.text = Debug;
     }
 }
